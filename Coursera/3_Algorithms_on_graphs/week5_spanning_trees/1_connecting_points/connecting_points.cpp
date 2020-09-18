883b1f5f80e76f1e6fd50f8e4d2f3049edd987ef
Prajwal Poojari <gprajwalpoojari@gmail.com>
2020-09-18 09:14:00 +0530

    Week 5 Assignment - Problems on Kruskal and Prim Algorithm

--- Coursera/3_Algorithms_on_graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
+++ Coursera/3_Algorithms_on_graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
@@ -3,12 +3,38 @@
 #include <iomanip>
 #include <vector>
 #include <cmath>
+#include <queue>
 
+using std::priority_queue;
 using std::vector;
+using std::pair;
+typedef pair<double, int> type;
 
+double calc_dist(vector<int> &x, vector<int> &y, int index_a, int index_b) {
+  return sqrt(pow(x[index_b] - x[index_a], 2) + pow(y[index_b] - y[index_a], 2));
+}
 double minimum_distance(vector<int> x, vector<int> y) {
   double result = 0.;
   //write your code here
+  vector<double> cost(x.size(), std::numeric_limits<double>::max());
+  vector<bool>road_built(x.size(), 0);
+  cost[0] = 0;
+  priority_queue<type, vector<type>, std::greater<type>> q;
+  q.push(std::make_pair(cost[0],0));
+  while(!q.empty()) {
+    type tp = q.top();
+    q.pop();
+    if (!road_built[tp.second]) {
+      road_built[tp.second] = 1;
+      result += tp.first;
+      for (int v = 0; v < road_built.size(); v++) {
+        if (!road_built[v] && cost[v] > calc_dist(x, y, tp.second, v)) {
+          cost[v] = calc_dist(x, y, tp.second, v);
+          q.push(std::make_pair(cost[v], v));
+        }
+      }
+    }
+  }
   return result;
 } 
 
--- Coursera/3_Algorithms_on_graphs/week5_spanning_trees/2_clustering/clustering.cpp
+++ Coursera/3_Algorithms_on_graphs/week5_spanning_trees/2_clustering/clustering.cpp
@@ -4,16 +4,74 @@
 #include <cassert>
 #include <vector>
 #include <cmath>
+#include <queue>
 
+using std::priority_queue;
 using std::vector;
 using std::pair;
+typedef pair<double, pair<int, int>> type;
 
+void make_set(vector<int> &parent, vector<int> &rank, int index) {
+  parent[index] = index;
+  rank[index] = 0;
+}
 
+int Find(vector<int> &parent, int v) {
+  if (parent[v] != v) {
+    parent[v] = Find(parent, parent[v]);
+  }
+  return parent[v];
+}
 
+void Union(vector<int> &parent, vector<int> &rank, int x, int y) {
+  int x_id = Find(parent, x);
+  int y_id = Find(parent, y);
+  if (x_id == y_id) return;
+  if (x_id < y_id) {
+    parent[x_id] = y_id;
+  }
+  else {
+    parent[y_id] = x_id;
+    if (x_id == y_id) {
+      rank[x_id] += 1;
+    }
+  }
+}
+
+double calc_dist(vector<int> &x, vector<int> &y, int a, int b) {
+  return sqrt(pow(x[b] - x[a], 2) + pow(y[b] - y[a], 2));
+}
 double clustering(vector<int> x, vector<int> y, int k) {
   //write your code here
-  return -1.;
-} 
+  vector<int> parent(x.size());
+  vector<int> rank(x.size());
+  for (int i = 0; i < x.size(); i++) {
+    make_set(parent, rank, i);
+  }
+  priority_queue<type, vector<type>, std::greater<type>> q;
+  for (int i = 0; i < x.size(); i++) {
+    for (int j = i + 1; j < y.size(); j++) {
+      q.push(std::make_pair(calc_dist(x, y, i, j), std::make_pair(i, j)));
+    }
+  }
+  int sets = x.size();
+  while (!q.empty()) {
+    type lightest = q.top();
+    q.pop();
+    int index1 = lightest.second.first;
+    int index2 = lightest.second.second;
+    if (Find(parent, index1) != Find(parent, index2)) {
+      if (sets != k) {
+        Union(parent, rank, index1, index2);
+        sets--;
+      }
+      else {
+        return lightest.first;
+      }
+    }
+  }
+  return 0;
+}
 
 int main() {
   size_t n;
